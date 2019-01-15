c4810e529401585dc642f4486ceca1d96b888c70
echeng <echeng@ci.com>
2019-01-15 09:47:23 -0500

    modify configuration to support deploy to tomcat.

--- src/main/java/personal/studio/manager/backend/Application.java
+++ src/main/java/personal/studio/manager/backend/Application.java
@@ -3,13 +3,14 @@ package personal.studio.manager.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import personal.studio.manager.backend.restful.RestfulAPIConfig;
 
 @SpringBootApplication//(exclude=DispatcherServletAutoConfiguration.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	
 
 
@@ -24,7 +25,8 @@ public class Application {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
         applicationContext.register(RestfulAPIConfig.class);
         dispatcherServlet.setApplicationContext(applicationContext);
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(dispatcherServlet, "/restful-api/*");
+        ServletRegistrationBean servletRegistrationBean =
+                new ServletRegistrationBean<>(dispatcherServlet, "/restful-api/*");
         servletRegistrationBean.setName("restful-api");
         return servletRegistrationBean;
     }
