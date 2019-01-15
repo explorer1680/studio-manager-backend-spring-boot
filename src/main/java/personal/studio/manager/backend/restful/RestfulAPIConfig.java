ad05b4f2a939adaa71f2e5250d97977e26875c7a
echeng <echeng@ci.com>
2019-01-15 13:37:46 -0500

    add angular project.

--- src/main/java/personal/studio/manager/backend/restful/RestfulAPIConfig.java
+++ src/main/java/personal/studio/manager/backend/restful/RestfulAPIConfig.java
@@ -3,9 +3,18 @@ package personal.studio.manager.backend.restful;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class RestfulAPIConfig {
+public class RestfulAPIConfig implements WebMvcConfigurer {
+
+        @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations("/");
+    }
 }
