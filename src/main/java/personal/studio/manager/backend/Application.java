b2dfc73f4d8db8f467f1a040af093e32e5194122
echeng <echeng@ci.com>
2019-01-17 14:12:06 -0500

    add login function.

--- src/main/java/personal/studio/manager/backend/Application.java
+++ src/main/java/personal/studio/manager/backend/Application.java
@@ -2,9 +2,16 @@ package personal.studio.manager.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import personal.studio.manager.backend.restful.RestfulAPIConfig;
@@ -18,6 +25,21 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
+        @Configuration
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/", "/home", "*.js", "/*.js", "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and().csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        }
+    }
 
     @Bean
     public ServletRegistrationBean foo() {
